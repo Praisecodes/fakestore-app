40682e726efe40db4f97784377ebbf2bf8059699
Praisecodes <cmancoder@gmail.com>
2022-10-26 22:18:50 +0100

    Minor changes

--- App.js
+++ App.js
@@ -11,9 +11,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
