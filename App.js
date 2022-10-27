cd8ae61179329be8f3fd8347095a11481f1e04b8
Praisecodes <cmancoder@gmail.com>
2022-10-27 11:55:06 +0100

    Attempting API call

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
+import React, {useEffect, useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 import Header from './components/header';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://fakestoreapi.com/products")
+    .then(res=>res.json())
+    .then((data)=>{
+      setProducts([...data]);
+      // console.log(products);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }, []);
+
+  const renderItem = ({item}) =>{
+    <Text style={styles.itemView}>{item.category}</Text>
+  }
   return (
     <>
     <StatusBar style='light' backgroundColor={'#223'} translucent={false} />
     <View style={styles.container}>
       <Header />
       <View style={styles.mainBody}>
-        <Text style={styles.text}>About To Fill This Space Up!!</Text>
+        {/* <Text style={styles.text}>About To Fill This Space Up!!</Text> */}
+        <FlatList 
+        data={products}
+        keyExtractor={item=>item.id}
+        renderItem={renderItem}
+        />
       </View>
     </View>
     </>
@@ -25,5 +49,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  itemView: {
+    width: wp('75%'),
+    backgroundColor: 'tomato',
+    paddingVertical: 10,
   }
 });
